36e6878a7564b768c5ebd9be94041e8bfdba08a6
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-15 13:48:33 +0200

    Add event listener to button

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,26 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-
 */
-// Select input elements
-const routeLengthInputElement = document.getElementById("route-length-field");
-const ageInputElement = document.getElementById("age-field");
+
+// Select elements
+const routeLengthField = document.getElementById("route-length-field");
+const ageField = document.getElementById("age-field");
+const buttonElement = document.querySelector("button");
 
 // Values
 const price_by_km = 0.21;
 const minor_discount = 0.2;
 const senior_discount = 0.4;
+
+let age;
+let distance;
+
+buttonElement.addEventListener("click", (event) => {
+	// Prevent refresh
+	event.preventDefault();
+
+	// Save values on click
+	age = ageField.value;
+	distance = routeLengthField.value;
+});
